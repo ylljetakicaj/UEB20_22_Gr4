61834548ced3844f0a0067a6d1a9246c05fdb5de
ylljetakicaj <118022552+ylljetakicaj@users.noreply.github.com>
2023-01-08 15:45:30 +0100

    update

--- darkmode.js
+++ darkmode.js
@@ -1,15 +1,23 @@
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
+const header = document.querySelector('header')
 
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high-fill')){
         body.style.background = 'white';
         body.style.color = 'black';
-       /* body.style.transition = '1s' */;
+
+       /* header.style.background = 'lightBlue';
+          header.style.color = 'grey'; */
+
+        body.style.transition = '1s';
     }else{
         body.style.background = 'black';
         body.style.color = 'white';
-       /* body.style.transition = '1s' */;
+    /* header.style.background = 'grey';
+        header.style.color = 'darkblue'; */
+
+         body.style.transition = '1s' ;
     }
 });
