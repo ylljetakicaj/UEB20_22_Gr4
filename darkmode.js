45629780280069198e862ec9b5cb7ffe1e2bafd9
Tringa Baftiu <97124034+Triinga@users.noreply.github.com>
2023-01-26 23:27:16 +0100

    update

--- darkmode.js
+++ darkmode.js
@@ -1,23 +1,16 @@
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
-const header = document.querySelector('header')
 
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high-fill')){
         body.style.background = 'white';
         body.style.color = 'black';
-
-       /* header.style.background = 'lightBlue';
-          header.style.color = 'grey'; */
-
+        header.style.color = 'grey';
         body.style.transition = '1s';
     }else{
         body.style.background = 'black';
         body.style.color = 'white';
-    /* header.style.background = 'grey';
-        header.style.color = 'darkblue'; */
-
-         body.style.transition = '1s' ;
+        body.style.transition = '1s' ;
     }
 });
--- home1.js
+++ home1.js
@@ -1,18 +1,15 @@
-
-
-
-
 const tabs = document.querySelectorAll('[data-tab-name]')
 const tabInfos = document.querySelectorAll('[data-tab-content]')
 
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
-        const target = document
-            .querySelector(tab.dataset.tabName);
+        const target = document.querySelector(tab.dataset.tabName); //elementi targert duhet me u shfaq kur tabi klikohet 
 
-        tabInfos.forEach(tabInfo => {
-            tabInfo.classList.remove('active')
+tabInfos.forEach(tabInfo => {
+            tabInfo.classList.remove('active') //removes active class from all elements 
         })
-        target.classList.add('active');
+        target.classList.add('active'); // to add the active class to the target element that was selected earlier, 
+        // makes the target element visible and the others invisible
+       
     })
 })
