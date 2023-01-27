d2210777bf4971dfd5c3df5cbee6dab97bf364b0
Ylljete Kicaj <118022552+ylljetakicaj@users.noreply.github.com>
2023-01-27 02:27:37 +0100

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -6,11 +6,12 @@ toggle.addEventListener('click', function(){
     if(this.classList.toggle('bi-brightness-high-fill')){
         body.style.background = 'white';
         body.style.color = 'black';
-        header.style.color = 'grey';
+ 
         body.style.transition = '1s';
     }else{
         body.style.background = 'black';
         body.style.color = 'white';
+        
         body.style.transition = '1s' ;
     }
 });
